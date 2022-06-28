8ce127efb565d93acb04e1fa41c23e69d7c9faeb
alicezhu2 <108353770+alicezhu2@users.noreply.github.com>
2022-06-28 11:25:44 -0400

    Update streamlit_app.py
    Added cutie emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.header("Alice here!")
+streamlit.title("My Parents New Healthy Dinner")
+
+streamlit.header('Breakfast Favorites')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avacado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
